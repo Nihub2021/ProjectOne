c81797dc35538184fccd7236a9b47e3ea84760f4
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-09-05 16:17:23 +0800

    port modified

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-function myFunction() {
+async function myFunction() {
+  console.log("launched");
+
   document.getElementById("demo").innerHTML = "script running";
 
+  const response = await fetch("http://localhost:5000/");
+  const data = await response.json();
+  console.log("bam", data.values);
 }
