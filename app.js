66e82e92dc1eee3d848c6edfee33b8b3fcb3f191
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-09-05 16:33:35 +0800

    add heroku fetch

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 async function myFunction() {
+
+  let url = "https://nihubrestapi.herokuapp.com/"
+
   console.log("launched");
 
   document.getElementById("demo").innerHTML = "script running";
 
-  const response = await fetch("http://localhost:5000/");
+  // const response = await fetch("http://localhost:5000/");
+  const response = await fetch(url);
   const data = await response.json();
   console.log("bam", data.values);
 }
